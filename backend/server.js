5f3ce6526c450f1fe57896419415059330f9423f
jeje-cloud <jeje181102@gmail.com>
2026-04-05 17:14:01 +0530

    Increased image size for backend

--- backend/server.js
+++ backend/server.js
@@ -8,8 +8,9 @@ const emotionRoutes = require('./routes/emotionRoutes');
 dotenv.config();
 
 const app = express();
-app.use(express.json());
-app.use(bodyParser.json());
+app.use(express.json({ limit: "50mb" }));
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 
 // Connect DB
 connectDB();
